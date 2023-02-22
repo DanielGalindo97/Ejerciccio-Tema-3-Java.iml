a138446ccbe948ab74c87cf44647d01fd6387042
DanielGalindo97 <126085326+DanielGalindo97@users.noreply.github.com>
2023-02-22 09:44:05 -0500

    .java
    Segunda parte:
    
    Crear una clase coche.
    
    Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
    
    Una función que incremente el número de puertas que tiene el coche.
    
    Crear un objeto miCoche en el main y añadirle una puerta.
    
    Mostrar el número de puertas que tiene el objeto.

--- .java
+++ .java
@@ -9,3 +9,20 @@
 +       System.out.println(suma(a,b,c));
 +    }
 + }
++
++
++ public class Main {
++    public static void main(String[] args) {
++        Coche micoche= new Coche();
++        micoche.MasPuerta();
++        System.out.println(micoche.puertas);
++    }
++
++ }
++ class Coche{
++   public int puertas=4;
++
++    public void MasPuerta() {
++        this.puertas++;
++    }
++ }
